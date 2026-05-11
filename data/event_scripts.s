1599fd8079cea4da9df268546c5da3ee4c29fdac
riceonyou <38494422+riceonyou@users.noreply.github.com>
2026-05-12 00:37:59 +0800

    modified:   data/event_scripts.s 	deleted:    data/layouts/Hoenn1/border.bin 	deleted:    data/layouts/Hoenn1/map.bin 	deleted:    data/layouts/Hoenn1_1_house1/border.bin 	deleted:    data/layouts/Hoenn1_1_house1/map.bin 	deleted:    data/layouts/Hoenn1_1_house1_2/border.bin 	deleted:    data/layouts/Hoenn1_1_house1_2/map.bin 	modified:   data/layouts/Hoenn1_2/map.bin 	modified:   data/layouts/Hoenn1_3/map.bin 	deleted:    data/layouts/Hoenn1_4/border.bin 	deleted:    data/layouts/Hoenn1_4/map.bin 	deleted:    data/layouts/Hoenn1_5/border.bin 	deleted:    data/layouts/Hoenn1_5/map.bin 	modified:   data/layouts/layouts.json 	modified:   data/maps/Hoenn1_1/map.json 	modified:   data/maps/Hoenn1_1/scripts.inc 	modified:   data/maps/Hoenn1_1/scripts.pory 	modified:   data/maps/Hoenn1_2/map.json 	modified:   data/maps/Hoenn1_2/scripts.inc 	new file:   data/maps/Hoenn1_3/map.json 	new file:   data/maps/Hoenn1_3/scripts.inc 	modified:   data/maps/map_groups.json 	modified:   include/constants/opponents.h 	modified:   src/data/pokemon/level_up_learnsets/gen_9.h 	modified:   src/data/pokemon/species_info/gen_3_families.h 	modified:   src/data/region_map/region_map_sections.json 	modified:   src/data/trainers.party

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1741,3 +1741,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/Hoenn1_1/scripts.inc"
 
 	.include "data/maps/Hoenn1_2/scripts.inc"
+
+	.include "data/maps/Hoenn1_3/scripts.inc"
--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -4,7 +4,7 @@
 #include "constants/battle_partner.h"
 #include "constants/opponents_frlg.h"
 
-#define TRAINER_NONE                          0
+#define TRAINER_HOENN1_2_TREECKO                          0
 #define TRAINER_SAWYER_1                      1
 #define TRAINER_GRUNT_AQUA_HIDEOUT_1          2
 #define TRAINER_GRUNT_AQUA_HIDEOUT_2          3
