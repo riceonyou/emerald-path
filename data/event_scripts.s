774dd8caa50cc671ac00ae591689d5bc617cd97a
riceonyou <38494422+riceonyou@users.noreply.github.com>
2026-05-30 14:53:49 +0800

    modified:   data/event_scripts.s 	new file:   data/layouts/Hoenn3_4/border.bin 	new file:   data/layouts/Hoenn3_4/map.bin 	modified:   data/layouts/layouts.json 	new file:   data/maps/Hoenn3_4/map.json 	new file:   data/maps/Hoenn3_4/scripts.inc 	modified:   data/maps/map_groups.json

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1771,3 +1771,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/Hoenn3_2/scripts.inc"
 
 	.include "data/maps/Hoenn3_3/scripts.inc"
+
+	.include "data/maps/Hoenn3_4/scripts.inc"
